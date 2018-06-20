8baa541a9b30f834781c5407e9e0695556d5e15e
Rahul Gupta <rahul.gupta18@infosys.com>
2018-06-20 15:03:12 +0530

    1. Created and added Constant class for managing constant values 2. Created and added Utility class for common function

---
+++ ProficiencyTest/ProficiencyTest/Utils/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  ProficiencyTest
+//
+//  Created by Mac_Admin on 20/06/18.
+//  Copyright © 2018 Infosys Ltd. All rights reserved.
+//
+
+import Foundation
+
+class Constants {
+    
+    static let  BaseURL =  "https://dl.dropboxusercontent.com"
+    static let  GetFacts =  "/s/2iodh4vg0eortkl/facts.json"
+}
---
+++ ProficiencyTest/ProficiencyTest/Utils/Utility.swift
@@ -0,0 +1,47 @@
+//
+//  Utility.swift
+//  ProficiencyTest
+//
+//  Created by Mac_Admin on 20/06/18.
+//  Copyright © 2018 Infosys Ltd. All rights reserved.
+//
+
+
+import Foundation
+import SystemConfiguration
+import UIKit
+
+class Utility {
+    
+    static func showAlert(title: String, message: String, buttonText: String, viewController: UIViewController)
+    {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: buttonText, style: UIAlertActionStyle.default, handler: nil))
+        viewController.present(alert, animated: true, completion: nil)
+    }
+    
+    static func isConnectedToNetwork() -> Bool {
+        
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(MemoryLayout<sockaddr_in>.size)
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        
+        guard let defaultRouteReachability = withUnsafePointer(to: &zeroAddress, {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
+                SCNetworkReachabilityCreateWithAddress(nil, $0)
+            }
+        }) else {
+            return false
+        }
+        
+        var flags: SCNetworkReachabilityFlags = []
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) {
+            return false
+        }
+        
+        let isReachable = flags.contains(.reachable)
+        let needsConnection = flags.contains(.connectionRequired)
+        
+        return (isReachable && !needsConnection)
+    }
+}
