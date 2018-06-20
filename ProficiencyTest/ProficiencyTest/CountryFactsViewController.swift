7c81f7486744a0fa1395f4e5edbc76a0e33f01a0
Rahul Gupta <rahul.gupta18@infosys.com>
2018-06-20 11:12:52 +0530

    Created ProficiencyTest project

---
+++ ProficiencyTest/ProficiencyTest/CountryFactsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  CountryFactsViewController.swift
+//  ProficiencyTest
+//
+//  Created by Mac_Admin on 20/06/18.
+//  Copyright © 2018 Infosys Ltd. All rights reserved.
+//
+
+import UIKit
+
+class CountryFactsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
