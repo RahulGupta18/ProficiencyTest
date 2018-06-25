4d5f82ec43c3e8f0cdf4641abf51af38b60a88f7
Rahul Gupta <rahul.gupta18@infosys.com>
2018-06-25 14:52:57 +0530

    Setting application root view controller

--- ProficiencyTest/ProficiencyTest/AppDelegate.swift
+++ ProficiencyTest/ProficiencyTest/AppDelegate.swift
@@ -12,10 +12,17 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        let vcCountry = CountryFactsViewController()
+        let navigationViewController = UINavigationController.init(rootViewController: vcCountry)
+        self.window?.rootViewController = navigationViewController
+        self.window?.makeKeyAndVisible()
+        
         return true
     }
 
